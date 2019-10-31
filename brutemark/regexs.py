a3a4f7f5940a2124722d6720065cb42aa3c7945b
devdave <devdave@ominian.net>
2019-10-31 14:21:04 -0600

    Relocate Regexs to their own module

---
+++ brutemark/regexs.py
@@ -0,0 +1,21 @@
+import re
+
+BLANK = re.compile(r"^$")
+
+CODELINE = re.compile(r"(^\ {4})|(^\t)")
+START_WS = re.compile(r"^(\s+)")
+QUOTED = re.compile(r"^(\>)")
+
+ORDERED_ITEM = re.compile(r"^\d{1,}\.") # (Numeric)(period)
+UNORDERED_ITEM = re.compile(r"^\* ")
+
+ANCHOR = re.compile(r"""\[([^\]]+)\]\(([^\)]+)\)|\[([^\]]+)\]\(([^\)]+)( "[^"]")\)""")
+IMAGE = re.compile(r"""\!\[([^\]]+)\]\(([^\)]+)\)|\!\[([^\]]+)\]\(([^\)]+)( "[^"]")\)""")
+
+STRONG_underscore = re.compile(r"""(\_{2}([^_]+)\_{2})""")
+STRONG_star = re.compile(r"""(\*{2}([^_]+)\*{2})""")
+EMPHASIS_underscore = re.compile(r"\_([^\_]+)\_")
+EMPHASIS_star = re.compile(r"(\*{2}([^\*]+)\*{2})")
+
+
+LINE_HEADER = re.compile(r"""^((\#+) )""")
