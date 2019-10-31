a17d6b0d3c67c32290d69715a1f8bee365e0394d
devdave <devdave@ominian.net>
2019-10-31 13:20:09 -0600

    Test emphasis

--- brutemark/tests/test_strong_emphasis_processing.py
+++ brutemark/tests/test_strong_emphasis_processing.py
@@ -1,4 +1,4 @@
-from brutemark.parser import TokenizeBody, Text, StrongText
+from brutemark.parser import TokenizeBody, Text, StrongText, EmphasisText
 
 
 def test_detects_strong_markdown():
@@ -9,3 +9,13 @@ def test_detects_strong_markdown():
 
     assert isinstance(actual[0], Text)
     assert isinstance(actual[1], StrongText)
+
+
+def test_detects_emphasis_markdown():
+
+    test = "Hello _World_"
+    expected = [Text("Hello "), EmphasisText("World")]
+    actual = TokenizeBody(test)
+
+    assert isinstance(actual[0], Text)
+    assert isinstance(actual[1], EmphasisText)
