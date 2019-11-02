1a589b8ea74d7cd1b1345ac84e9ebbd16a9a3965
devdave <devdave@ominian.net>
2019-11-02 08:11:44 -0600

    Somehow all tests are passing

--- brutemark/tests/test_strong_emphasis_processing.py
+++ brutemark/tests/test_strong_emphasis_processing.py
@@ -1,10 +1,30 @@
 from brutemark.parser import TokenizeBody, Text, StrongText, EmphasisText
 
 
+def test_emphasis_consume_matches():
+    test = "Hello _World_"
+    expected = EmphasisText("World",6,13)
+    actual_text, actual_token = EmphasisText.Consume(test) # type: EmphasisText
+
+    assert actual_token is not None
+    assert actual_token.start == expected.start
+    assert actual_token.content == expected.content
+    assert actual_token.stop == expected.stop
+
+def test_strong_consumes_match():
+    test = "Hello __World__"
+    expected = StrongText("World",6,15)
+    actual_text, actual_token = StrongText.Consume(test)
+
+    assert actual_token is not None
+    assert actual_token.start == expected.start
+    assert actual_token.stop == expected.stop
+    assert actual_token.content == expected.content
+
 def test_detects_strong_markdown():
 
     test = "Hello __world__"
-    expected = [Text("Hello "), StrongText("world")]
+    expected = [Text("Hello ",0,6), StrongText("world",7,13)]
     actual = TokenizeBody(test)
 
     assert isinstance(actual[0], Text)
@@ -14,43 +34,41 @@ def test_detects_strong_markdown():
 def test_detects_emphasis_markdown():
 
     test = "Hello _World_"
-    expected = [Text("Hello "), EmphasisText("World")]
+    expected = [Text("Hello ",0,6), EmphasisText("World",7,13)]
     actual = TokenizeBody(test)
 
     assert isinstance(actual[0], Text)
     assert isinstance(actual[1], EmphasisText)
 
 
+def test_emphasis_consumes_nested_strongtext():
+    test = "_Hello **World**_"
+    expected_content = "Hello **World**"
+    expected = EmphasisText("Hello **World**", 0, 17)
+    actual_text, actual_token = EmphasisText.Consume(test)
+
+    assert actual_token is not None
+    assert actual_token.content == expected_content
+    assert actual_token.start == expected.start
+    assert actual_token.stop == expected.stop
+
+
+
 def test_strong_nested_in_emphasis():
-    """
-    So this one is a bit more complicated and may cause a drastic change to the tokenizer logic.
-
-        Nested markup creates a problem where
-            MarkupA and MarkupB can inversely nest each other.
-
-    __Foo **bar** string__ is easier based on processor precedence
-    but
-    *Foo __bar__ string* complicates things.
-
-    Another problem would be goofy URLS for Anchor and Image tokens
-    [Foo](http://server.tld/foo__bar__url.html "Title string")
-    and
-    [Foo](http://server.tld/foo_bar_url.html "Title string")
-    would be caught by Strong and Emphasis body tokens which would be inappropriate
-
-    Switching to a lower level tokenizer with
-        ^__Hello **World**__ this is a link to [Markdown](link_rel "title")
-    TO
-        [EmphasisToken,Hello, ,StrongToken,World, ,this, ,is, ,a, ,link, ,to, ,ANCHOR_BODY,Markdown,ANCHOR_BODY,ANCHOR_HREF,link_rel, ,"Title",ANCHOR_HREF]
-    would create a lot of problems.
-
-    
-    :return:
-    """
 
     test = "_Hello **World**_"
-    expected = [EmphasisText([Text("Hello "),StrongText("World")])]
+    expected = [
+            EmphasisText(
+                [
+                    Text("Hello ",0,6),
+                    StrongText("World", 0 , 7)
+                ],
+                0,17
+            )
+    ]
+
     actual = TokenizeBody(test)
 
     assert len(actual) == 1
-    assert actual[0].content == expected[0].content
+    assert isinstance(actual[0], EmphasisText)
+    assert actual[0].content[0].content == expected[0].content[0].content
