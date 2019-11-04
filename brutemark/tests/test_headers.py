9e41ca9cae4312c52d66a284223c116b16b2b94f
devdave <devdave@ominian.net>
2019-11-04 13:58:53 -0700

    Verify content is correct for header lines

--- brutemark/tests/test_headers.py
+++ brutemark/tests/test_headers.py
@@ -36,3 +36,14 @@ def test_all_nested_headers():
         assert isinstance(token, HeaderLine)
         assert token.level == position + 1, f"Expected {position+1} for level but got {token.level} with {test_str!r}"
         assert token.nested == True
+
+def test_header_content_is_correct():
+    for position, test_str in enumerate(tests):
+        token = TokenizeLine(test_str)
+        assert token.content == f"h{token.level}"
+
+
+def test_headerline_consumes_text_correctly():
+
+    _, actual = HeaderLine.TestAndConsume("## h2")
+    assert actual.content == "h2"
